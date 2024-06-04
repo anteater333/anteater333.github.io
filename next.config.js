80585bf832301639e76fd47ab4bc90a8fad09413
anteater@mac <anteater1056@gmail.com>
2024-06-04 17:13:23 +0900

    개발 환경 세팅 중...

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   compiler: {
     styledComponents: true,
+  },
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,21 @@
 import { remark } from "remark";
-import html from "remark-html";
+import gfm from "remark-gfm";
+import directive from "remark-directive";
+import rehype from "remark-rehype";
+import raw from "rehype-raw";
+import slug from "rehype-slug";
+import pretty from "rehype-pretty-code";
+import stringify from "rehype-stringify";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(gfm)
+    .use(directive)
+    .use(rehype)
+    .use(raw)
+    .use(slug)
+    .use(pretty)
+    .use(stringify)
+    .process(markdown);
   return result.toString();
 }
