a3008d463a4f057c9725e9505ba4368b40f3af5b
anteater@mac <anteater1056@gmail.com>
2024-06-08 14:20:34 +0900

    kramdown식 attribute list 적용 라이브러리 설치

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,6 +1,7 @@
 import { remark } from "remark";
 import gfm from "remark-gfm";
 import directive from "remark-directive";
+import attributeList from "remark-attribute-list";
 import rehype from "remark-rehype";
 import raw from "rehype-raw";
 import slug from "rehype-slug";
@@ -11,6 +12,7 @@ export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(gfm)
     .use(directive)
+    .use(attributeList)
     .use(rehype)
     .use(raw)
     .use(slug)
