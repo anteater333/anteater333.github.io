1b01a9d2691b474cc5407c6475b4e29ae0dae648
anteater@mac <anteater1056@gmail.com>
2024-06-14 15:02:59 +0900

    Markdown watcher 구현

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,8 @@ export const HOME_OG_IMAGE_URL =
   "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
 
 export const CATEGORIES = ["meta", "micro", "hack", "reddit", "memoir", "ndev"];
+
+/**
+ * 기존 ver.2에선 35729 포트 사용 중이었음
+ */
+export const WS_PORT = 35730;
