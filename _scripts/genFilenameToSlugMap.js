f402e011157204a97a53597dac8cb828297ba481
anteater@mac <anteater1056@gmail.com>
2024-06-28 10:36:13 +0900

    설명을 위한 텍스트 조금 추가

--- _scripts/genFilenameToSlugMap.js
+++ _scripts/genFilenameToSlugMap.js
@@ -1,3 +1,14 @@
+/**
+ * genFilenameToSlugMap
+ * _posts/ 경로 아래의 .md 파일들을 읽어 slug 정보를 추출한다.
+ * 기존 블로그에서 사용하던 .md 파일 명명법 규칙을 유지하면서
+ * 새로운 방식의 Slug 규칙을 URL에 적용하기 위해 이런 방식을 사용함.
+ *
+ * 스크립트 실행의 결과물로 다음 두 파일이 생성된다
+ * ./ref/filenameToSlug.json
+ * ./ref/slugToFilename.json
+ */
+
 const { readdir, readdirSync, writeFileSync, readFileSync } = require("fs");
 const { join } = require("path");
 const matter = require("gray-matter");
