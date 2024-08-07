d665e7e032b3fd6818cbfd44eb1c603a5529796b
anteater@mac <anteater1056@gmail.com>
2024-08-07 17:42:48 +0900

    ToC 컴포넌트 레이아웃
    아직 강조 효과 기능 구현 필요

--- src/app/_components/PostBody.tsx
+++ src/app/_components/PostBody.tsx
@@ -1,62 +1,155 @@
 "use client";
 
-import { rainbowColor, textBackgroundTransition } from "@/styles/values";
+import {
+  rainbowColor,
+  scOnHalf,
+  scOnPalm,
+  textBackgroundTransition,
+} from "@/styles/values";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const PostBodySection = styled.section`
-  width: 100%;
-  color: #222222;
-  font-weight: normal;
-
-  h2 {
-    @media screen and (max-width: $on-half) {
-      font-size: 2rem;
-    }
-    @media screen and (max-width: $on-palm) {
-    }
-
-    font-size: 2.4rem;
-    word-break: keep-all;
+const PostToCNav = styled.nav`
+  @media ${scOnHalf} {
+    width: 16rem;
+    min-width: 16rem;
+  }
+  @media ${scOnPalm} {
+    display: none;
   }
 
-  a {
-    background: ${rainbowColor};
-    background-size: 400% 100%;
-    -webkit-background-clip: text;
-    background-clip: text;
+  width: 18rem;
+  min-width: 18rem;
 
-    -webkit-transition: color 0.2s;
-    transition: color 0.2s;
+  margin-left: 1rem;
 
-    color: #6666ff;
+  .toc-core {
+    position: sticky;
+    top: 1rem;
 
-    &:hover {
-      animation: ${textBackgroundTransition} 6s ease-in-out infinite;
-      color: transparent;
-    }
-    &:active {
-      animation: none;
-      color: lighten(#222222, 50%);
+    border-left: 4px solid #222222;
+
+    padding-left: 0.75rem;
+
+    li {
+      display: flex;
+
+      margin-bottom: 0.5rem;
+      > p.toc-indent {
+        margin: 0 0 0 1rem;
+      }
     }
   }
 `;
 
-const PostToCSection = styled.section``;
+type ToCItem = {
+  text: string;
+  id: string;
+  level: number;
+};
+
+function PostToC({ headings }: { headings: ToCItem[] }) {
+  return (
+    <PostToCNav className="toc-rail">
+      <ul className="toc-core">
+        {headings.map((heading, idx) => {
+          return (
+            <li key={`post-toc-item-${idx}`}>
+              {Array.from(Array(heading.level), (e, jdx) => {
+                return (
+                  <p
+                    className="toc-indent"
+                    key={`toc-indent-${idx}-${jdx}`}
+                  ></p>
+                );
+              })}
+              <a href={`#${heading.id}`}>{heading.text}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </PostToCNav>
+  );
+}
+
+const PostBodySection = styled.section`
+  display: flex;
+
+  .post-content {
+    min-width: 0;
+
+    color: #222222;
+    font-weight: normal;
+
+    h2 {
+      @media ${scOnHalf} {
+        font-size: 2rem;
+      }
+      @media ${scOnPalm} {
+      }
+
+      font-size: 2.4rem;
+      word-break: keep-all;
+    }
+
+    img {
+      width: 100%;
+      object-fit: contain;
+    }
+
+    a {
+      background: ${rainbowColor};
+      background-size: 400% 100%;
+      -webkit-background-clip: text;
+      background-clip: text;
+
+      -webkit-transition: color 0.2s;
+      transition: color 0.2s;
+
+      color: #6666ff;
+
+      &:hover {
+        animation: ${textBackgroundTransition} 6s ease-in-out infinite;
+        color: transparent;
+      }
+      &:active {
+        animation: none;
+        color: lighten(#222222, 50%);
+      }
+    }
+  }
+`;
 
 export function PostBody({ content }: { content: string }) {
+  const [headings, setHeadings] = useState<ToCItem[]>([]);
+
+  /** content로부터 headings 추출 (for ToC) */
+  useEffect(() => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(content, "text/html");
+
+    const headingEls = doc.querySelectorAll("h1, h2, h3, h4, h5, h6");
+    const buffer: ToCItem[] = [];
+    headingEls.forEach((el) => {
+      buffer.push({
+        text: el.textContent ?? "",
+        id: el.id,
+        level: +el.tagName.split("H")[1] - 2,
+      });
+    });
+
+    setHeadings(buffer);
+  }, []);
+
   return (
     <PostBodySection>
       <div
         className="post-content"
         dangerouslySetInnerHTML={{ __html: content }}
       />
-      <PostToC />
+      <PostToC headings={headings} />
     </PostBodySection>
   );
-}
-
-function PostToC({}) {
-  return <PostToCSection></PostToCSection>;
 }
 
 export default PostBody;
