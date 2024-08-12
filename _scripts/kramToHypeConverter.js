5ce43b3067d683d6b8dd15a08458639cd4fe6aff
anteater@mac <anteater1056@gmail.com>
2024-08-12 17:34:57 +0900

    Kramdown to HTML 스크립트 보완
    a 태그도 이제 자동 수정

--- _scripts/kramToHypeConverter.js
+++ _scripts/kramToHypeConverter.js
@@ -5,9 +5,11 @@ const output = [];
  * @param {string} source
  */
 function imgWithoutComment(source) {
-  const regexImgWithoutComment = /\!\[.*\]\(.*\)[ ]{0,}[\r\n]*{:( \.[^ ]+)+}/g;
+  output.push(`\n- imgWithoutComment:`);
 
-  const found = source.match(regexImgWithoutComment);
+  const regex = /\!\[.*\]\(.*\)[ ]{0,}[\r\n]*{:( \.[^ ]+)+}/g;
+
+  const found = source.match(regex);
 
   if (!found) return source;
 
@@ -41,10 +43,11 @@ function imgWithoutComment(source) {
  * @param {string} source
  */
 function imgWithComment(source) {
-  const regexImgWithoutComment =
-    /\!\[.*\]\(.*\)[ ]{0,}[\r\n]*.+[\r\n]*{:( \.[^ ]+)+}/g;
+  output.push(`\n- imgWithComment:`);
 
-  const found = source.match(regexImgWithoutComment);
+  const regex = /\!\[.*\]\(.*\)[ ]{0,}[\r\n]*.+[\r\n]*{:( \.[^ ]+)+}/g;
+
+  const found = source.match(regex);
 
   if (!found) return source;
 
@@ -76,8 +79,38 @@ function imgWithComment(source) {
   return source;
 }
 
+/**
+ *
+ * @param {string} source
+ */
+function aTag(source) {
+  output.push(`\n- aTag:`);
+
+  const regex = /\[(.*?)\]\((.*?)\)/g;
+
+  const found = source.match(regex);
+
+  if (!found) return source;
+
+  const replaced = found.map((text) => {
+    const content = text.split("[")[1].split("]")[0];
+    const link = text.split("(")[1].split(")")[0];
+
+    output.push(content);
+
+    return `<a href="${link}">${content}</a>`;
+  });
+
+  found.forEach((preVal, index) => {
+    const newVal = replaced[index];
+    source = source.replace(preVal, newVal);
+  });
+
+  return source;
+}
+
 const fs = require("fs");
-const { join } = require("path");
+const { join, resolve } = require("path");
 
 function main() {
   const target = "../_posts/";
@@ -87,11 +120,11 @@ function main() {
     output.push(`\n= ${target} =\n`);
     fs.mkdirSync(join(dest, subdir), { recursive: true });
     fs.readdirSync(join(target, subdir)).forEach((mdFile) => {
-      output.push(`== ${mdFile}`);
+      output.push(`== ${resolve(mdFile)}`);
 
       const source = fs.readFileSync(join(target, subdir, mdFile), "utf-8");
 
-      const result = imgWithComment(imgWithoutComment(source));
+      const result = aTag(imgWithComment(imgWithoutComment(source)));
 
       fs.writeFileSync(join(dest, subdir, mdFile), result);
 
@@ -105,7 +138,9 @@ function main() {
 function test() {
   const target = "../_posts/hack/2022-03-07-hack-the-terms-1.md";
 
-  imgWithComment(fs.readFileSync(target, "utf8"));
+  aTag(imgWithComment(fs.readFileSync(target, "utf8")));
+
+  console.log(output.join("\n"));
 }
 
 // test();
