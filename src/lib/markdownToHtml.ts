df2c5e789f72e7d3a4f1b391d2e50d0888307ffa
anteater@mac <anteater1056@gmail.com>
2024-08-13 12:07:29 +0900

    remark-gfm 제거

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,5 +1,4 @@
 import { remark } from "remark";
-import gfm from "remark-gfm";
 import rehype from "remark-rehype";
 import raw from "rehype-raw";
 import slug from "rehype-slug";
@@ -9,7 +8,6 @@ import externalLinks from "rehype-external-links";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
-    .use(gfm)
     .use(rehype, { allowDangerousHtml: true })
     .use(slug)
     .use(raw)
