444e656310ab30758c11498302e74ffacdc7db5b
anteater@mac <anteater1056@gmail.com>
2024-10-14 15:24:44 +0900

    registry 기능까지 다시 만들어보면서 생각 좀 정리

--- _testbed/241008_nextssg/app/page.tsx
+++ _testbed/241008_nextssg/app/page.tsx
@@ -1,9 +1,11 @@
+import Indicator from "./_components/Indicator";
 import ToggleButton from "./_components/ToggleButton";
 
 export default function Home() {
   return (
     <div className="main-container">
       <ToggleButton />
+      <Indicator />
     </div>
   );
 }
