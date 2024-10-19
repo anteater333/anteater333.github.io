a6f0fedb8b51d0998abffd31e7f9b609652f2203
anteater@mac <anteater1056@gmail.com>
2024-10-19 12:35:21 +0900

    광고 배너 스타일링 및 리소스 적용

--- src/app/_components/AdBanner.tsx
+++ src/app/_components/AdBanner.tsx
@@ -5,30 +5,72 @@ import styled from "styled-components";
 
 const AdBannerDiv = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  margin-top: 1rem;
-  margin-bottom: 1rem;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+
+  span {
+    font-size: 0.75rem;
+    margin-bottom: 0.1rem;
+  }
 
   .ad-banner-container {
     width: 75%;
     height: 4rem;
     max-width: 20rem;
-    background-color: red;
+    background-color: var(--static-black);
     border-radius: 0.5rem;
+    overflow: hidden;
+
+    .ad-banner-placeholder {
+      width: 100%;
+      height: 100%;
+
+      background-color: var(--bg-color-main);
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+
+      object-fit: cover;
+    }
   }
 `;
 
 const adItems = [
-  { id: 0, image: "", link: "" },
-  { id: 1, image: "", link: "" },
-  { id: 2, image: "", link: "" },
-  { id: 3, image: "", link: "" },
+  {
+    title: "domado",
+    image: "/assets/pictures/ads/ad-1-domado.png",
+    link: "https://domado.vercel.app/",
+  },
+  {
+    title: "soup",
+    image: "/assets/pictures/ads/ad-2-soup.png",
+    link: "https://blog.anteater-lab.link/namu-soup/",
+  },
+  {
+    title: "watchunduck",
+    image: "/assets/pictures/ads/ad-3-watchunduck.png",
+    link: "https://watchunduck.tistory.com/",
+  },
+  {
+    title: "buymeacoffee",
+    image: "/assets/pictures/ads/ad-4-bmc.png",
+    link: "http://buymeacoffee.com/anteater333",
+  },
 ];
 
 export default function AdBanner() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+
+  /** 초기 인덱스 랜덤 설정 */
+  useEffect(() => {
+    setCurrentIndex(Math.floor(Math.random() * adItems.length));
+  }, []);
 
   const nextAd = useCallback(() => {
     setCurrentIndex((prevIndex) => {
@@ -48,16 +90,22 @@ export default function AdBanner() {
     return () => clearInterval(interval);
   });
 
+  useEffect(() => {
+    console.log(currentIndex);
+  }, [currentIndex]);
+
   return (
     <AdBannerDiv>
       <span>sponsored by</span>
       <div className="ad-banner-container">
-        <a href={adItems[currentIndex].link}>
-          <img src={adItems[currentIndex].image} />
-        </a>
+        {currentIndex < 0 ? (
+          <div className="ad-banner-placeholder"></div>
+        ) : (
+          <a href={adItems[currentIndex].link}>
+            <img src={adItems[currentIndex].image} />
+          </a>
+        )}
       </div>
-      <button onClick={prevAd}>이전</button>
-      <button onClick={nextAd}>다음</button>
     </AdBannerDiv>
   );
 }
