036fefe1aea5ab218ef476baf7eff83c129c6582
anteater@mac <anteater1056@gmail.com>
2024-10-24 18:02:39 +0900

    미사용 코드 제거 (testbed)

--- _testbed/241008_nextssg/app/layout.tsx
+++ _testbed/241008_nextssg/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { DarkModeRegistry } from "./registry";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,9 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <DarkModeRegistry>
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
-          {children}
-        </body>
+        <body>{children}</body>
       </DarkModeRegistry>
     </html>
   );
