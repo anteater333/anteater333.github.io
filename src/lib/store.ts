83ec7fb67b21fb3ebf32ec49e6c1a60ba66f6b67
anteater@mac <anteater1056@gmail.com>
2024-11-06 16:54:37 +0900

    웹 사이트 테마 전역 상태를 스토리지에 저장
    서버측 렌더링에서 오류가 존재하기 때문에 수정 필요

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type Store = {
   isDarkMode: boolean;
@@ -7,13 +8,22 @@ type Store = {
   setIsDarkMode: (isDarkMode: boolean) => void;
 };
 
-const useStore = create<Store>()((set) => {
-  return {
-    isDarkMode: false,
-    toggleDark: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
-    setIsDarkMode: (isDarkMode) => set((state) => ({ isDarkMode })),
-  };
-});
+const useStore = create<Store>()(
+  persist(
+    (set) => ({
+      isDarkMode:
+        typeof window !== "undefined" &&
+        typeof localStorage.getItem("theme-storage") === "string"
+          ? JSON.parse(localStorage.getItem("theme-storage") as string)[
+              "state"
+            ]["isDarkMode"]
+          : false,
+      toggleDark: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
+      setIsDarkMode: (isDarkMode) => set((state) => ({ isDarkMode })),
+    }),
+    { name: "theme-storage" }
+  )
+);
 
 export const useMatchMedia = () => {
   const { setIsDarkMode } = useStore();
@@ -27,8 +37,9 @@ export const useMatchMedia = () => {
       setIsDarkMode(e.matches);
     };
 
-    // 초기 상태 설정
-    setIsDarkMode(mediaQueryList.matches);
+    // 로컬 스토리지에 저장된 테마 상태값이 없다면 시스템 설정을 가져온다.
+    if (!localStorage.getItem("theme-storage"))
+      setIsDarkMode(mediaQueryList.matches);
 
     // 리스너 등록
     mediaQueryList.addEventListener("change", updateDarkMode);
