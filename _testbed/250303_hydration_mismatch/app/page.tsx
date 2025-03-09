86a67e991276ff29afeea86f72cdc33ddb0c7273
Jihoon Lee <anteater1056@gmail.com>
2025-03-10 08:37:46 +0900

    tip 22, 생각 정리

--- _testbed/250303_hydration_mismatch/app/page.tsx
+++ _testbed/250303_hydration_mismatch/app/page.tsx
@@ -1,12 +1,10 @@
 import styles from "./page.module.css";
 import DateString from "./DateString";
-import ThemeToggle from "./ThemeToggle";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <DateString />
-      <ThemeToggle />
     </div>
   );
 }
