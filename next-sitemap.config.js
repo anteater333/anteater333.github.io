202e06684dee81bccf1f0e43cbbaaee62695c455
Jihoon Lee <anteater1056@gmail.com>
2025-04-13 16:04:18 +0900

    추가 사이트맵

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,7 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://blog.anteater-lab.link",
   generateRobotsTxt: true,
   outDir: "out/",
+  robotsTxtOptions: {
+    additionalSitemaps: ["https://blog.anteater-lab.link/sitemap-sub.xml"],
+  },
 };
