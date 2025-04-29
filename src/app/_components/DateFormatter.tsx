6c45eda0cc258e3ebfd0cb2f1a39343ebc43d79b
Jihoon Lee <anteater1056@gmail.com>
2025-04-30 08:14:37 +0900

    자잘한 리팩토링
    리액트 개발자 도구의 프로파일러 사용 중

--- src/app/_components/DateFormatter.tsx
+++ src/app/_components/DateFormatter.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { format } from "date-fns";
+import { memo } from "react";
 
 type Props = {
   dateString: string;
 };
 
-const DateFormatter = ({ dateString }: Props) => {
+const DateFormatter = memo(({ dateString }: Props) => {
   // Safari의 parsing 알고리즘이 유독 저열하기 때문에 전처리
   const date = new Date(
     Date.parse(dateString.replace(" ", "T").replace(" ", ""))
@@ -17,6 +18,6 @@ const DateFormatter = ({ dateString }: Props) => {
       {format(date, "LLLL	d, yyyy")}
     </time>
   );
-};
+});
 
 export default DateFormatter;
